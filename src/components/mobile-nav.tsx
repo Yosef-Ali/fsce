fa5f0a9902ad13968f7254693cfc628de188a975
Yosef-Ali <yosethio@yahoo.com>
2024-07-05 11:23:46 +0300

    logo

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -5,6 +5,7 @@ import { MainNavItem } from "@/types"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
+import Logo from "./logo"
 
 interface MobileNavProps {
   items: MainNavItem[]
@@ -20,7 +21,8 @@ export function MobileNav({ items, children }: MobileNavProps) {
     >
       <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
         <Link href="/" className="flex items-center space-x-2">
-          <Icons.logo />
+          {/* <Icons.logo /> */}
+          <Logo />
           <span className="font-bold">{siteConfig.name}</span>
         </Link>
         <nav className="grid grid-flow-row auto-rows-max text-sm">
