5df53b6dd81a8ffa10ef96e12a34ad0bb59a396e
Yosef-Ali <yosethio@yahoo.com>
2024-07-11 10:04:33 +0300

    categories update

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,32 +1,4 @@
-// // components/Categories.tsx
-// import React, { FC } from 'react';
 
-// const Categories: FC = () => {
-//   const categories = [
-//     { name: 'Programs', postCount: 2 },
-//     { name: 'Events', postCount: 10 },
-//     { name: 'News', postCount: 7 },
-//     { name: 'Success Stores', postCount: 4 }
-//   ];
-
-//   return (
-//     <div>
-//       <h2 className="scroll-m-20 text-xl font-semibold tracking-tight text-center">Categories</h2>
-//       <ul className="divide-y divide-gray-200">
-//         {categories.map((category, index) => (
-//           <li key={index} className="flex">
-//             <a href="" className="leading-7 [&:not(:first-child)]:mt-6 py-2 block flex-1">
-//               {category.name}
-//             </a>
-//             <span className="text-gray-700 text-lg font-thin p-2">{category.postCount}</span>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Categories;
 
 import { useQuery } from 'convex/react';
 import React from 'react';
@@ -42,7 +14,7 @@ const Categories = () => {
         <p>Loading categories...</p>
       ) : (
         <ul className="divide-y divide-gray-200">
-          {categories.map((category, index) => (
+          {categories.map((category: { name: string; postCount: number }, index) => (
             <li key={index} className="flex">
               <a href={`/category/${category.name.toLowerCase()}`} className="leading-7 [&:not(:first-child)]:mt-6 py-2 block flex-1">
                 {category.name}
